8f03c55cc3fa48c7b3fb19c6da1d6035002cecb8
Siddhartha Banerjee <banerjs.sid@gmail.com>
2019-06-11 16:33:05 -0400

    Update docker script to not error out on multiple consecutive runs

--- scripts/launch_docker.sh
+++ scripts/launch_docker.sh
@@ -4,5 +4,11 @@
 set -e
 
 MARY_VOICE=${1:-dfki-prudence-hsmm}
+CONTAINER_NAME=${2:-marytts}
 
-docker run -d --rm --name marytts -p 59125:59125 gtrail/marytts:${MARY_VOICE}
+if [ -z $(docker ps -q -f "name=${CONTAINER_NAME}") ]; then
+    echo "Creating the docker container ${CONTAINER_NAME} from gtrail/marytts:${MARY_VOICE}..."
+    docker run -d --rm --name ${CONTAINER_NAME} -p 59125:59125 gtrail/marytts:${MARY_VOICE}
+else
+    echo "Container with name ${CONTAINER_NAME} exists. Doing nothing..."
+fi
